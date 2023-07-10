6671cef5ca25d5e4a6098167d9c05f83d19ff7fe
Muguai <fredrik-hammar@hotmail.com>
2023-07-10 14:29:09 +0200

    feat: Done with mini Assignment. Forgot to commit since it went so fast :clown_face:

---
+++ MiniAssignmentBlogWebsite/js/index.js
@@ -0,0 +1,95 @@
+import { postArray } from "../data/posts.js";
+
+const elPosts = document.getElementById('posts');
+const elTopPost = document.getElementById('topPost');
+const elTopPostImage = document.getElementById('topPostImage');
+
+function createTopPost(post){
+    console.log("before " + elTopPost);
+    console.log("CreateTopPost " + post.Id);
+    elTopPostImage.src = post.photo;
+    const headerHTML = `<div class="card-header"> <b class="card-text  text-black-50">Published: ${post.date}</b> </div>`;
+    const bodyHTML = `<div class="card-body">
+
+    <h2 class="card-text"> ${post.title}</h2>
+    <h4 class="card-text card-subtitle text-black-50 mb-4">${post.subTitle}</h4>
+    <p>
+        ${post.intro}
+    </p>
+    <p class="text-black-50 d-flex align-items-center">
+        <span class="material-icons">person</span>
+        ${post.author}
+    </p>
+    </div>`;
+
+    console.log(createTag(post.tags))
+    elTopPost.innerHTML = headerHTML + bodyHTML + createTag(post.tags);
+}
+
+
+function createTag(tags){
+    let result = `<div class="card-footer ">`;
+    tags.forEach(element => (result += `<span class="badge bg-primary ">#${element}</span> `));
+    result += `</div>`;
+    return result;
+
+}
+
+function createPost(post){
+
+    const imgHTML = `<img class= "d-block w-100 card-img-top" src=${post.photo}> `;
+    const headerHTML = `<div class="card-header">
+                            <b class="card-text  text-black-50">Published: ${post.date}</b>
+                        </div> `;
+    const bodyHTML = `<div class="card-body">
+                        <h2 class="card-text">${post.title}</h2>
+                        <h4 class="card-text card-subtitle text-black-50 mb-4">${post.subTitle}</h4>
+                        <p>
+                            ${post.intro}
+                        </p>
+                        <p class="text-black-50 d-flex align-items-center">
+                            <span class="material-icons">person</span>
+                            ${post.author}
+                        </p>
+                    </div>`;
+    
+    return imgHTML + headerHTML + bodyHTML + createTag(post.tags);
+}
+
+function initializeWebsite(){
+    createTopPost(postArray[0]);
+    console.log(elPosts);
+    const sections = elPosts.getElementsByTagName('section');
+    let divArray = [];
+    for (var i = 0; i < sections.length; i += 1) {
+            console.log(i);
+            sections[i].innerHTML = createPost(postArray[i + 1]);
+    }
+}
+
+initializeWebsite();
+
+/*
+                    <img class= "d-block w-100 card-img-top" src="img/pexels-dana-tentis-691114.jpg">
+
+                    <div class="card-header">
+                        <b class="card-text  text-black-50">Published: 20 MAR 2021</b>
+                    </div>
+                    <div class="card-body">
+
+                        <h2 class="card-text">Title of article</h2>
+                        <h4 class="card-text card-subtitle text-black-50 mb-4">SubTitle</h4>
+                        <p>
+                            Blah blah balahbalhbalhbalhlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+                        </p>
+                        <p class="text-black-50 d-flex align-items-center">
+                            <span class="material-icons">person</span>
+                            Written by anonymous
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <span class="badge bg-primary">#random</span>
+                        <span class="badge bg-primary">#dumb</span>
+                        <span class="badge bg-primary">#pointless</span>
+                    </div>
+*/
